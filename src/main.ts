ac83e65468891d41707792c90fb9166d54ce02a7
maher <maher.ahmed@engaz.ai>
2026-02-23 13:12:45 +0200

    feat: Initialize NestJS application with Express and global validation.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.setGlobalPrefix('api');
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  });
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
 }
 bootstrap();
+
+export default server;
