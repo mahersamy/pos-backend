c89e875a5db395a1d7e14e4620aa850a14eb5bb1
maher <maher.ahmed@engaz.ai>
2026-02-24 18:39:49 +0200

    feat: Initialize NestJS application with global prefix, URI versioning, CORS, and global validation pipe.

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+  app.enableCors({
+    origin: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
