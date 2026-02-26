c2dc18bfd872005f3be9f3c6b40d80acb86f803b
maher <maher.ahmed@engaz.ai>
2026-02-26 03:07:27 +0200

    feat: initialize NestJS application with global prefix, URI versioning, CORS, and global validation pipes.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', { exclude: ['/'] });
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
